174226ecfa9867722fcd59486503d7fd0b7efbb3
lucassantosgomes02 <144077958+lucassantosgomes02@users.noreply.github.com>
2025-03-29 13:08:38 -0300

    test: teste03

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 app.get('/api', (req, res) => {
-  res.send('Hello Lucas');
+  res.send('Hello World!');
 });
 
 app.get('/api/test', (req, res) => {
